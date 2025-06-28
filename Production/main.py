7c313769f27930ac9de6ca05be0ba25081b327eb
maple1220 <mapletay1220@gmail.com>
2025-06-28 17:09:56 -0400

    Update main.py

--- Production/main.py
+++ Production/main.py
@@ -1,1 +1,23 @@
+main.py
+import board
 
+# These are imports from the kmk library
+from kmk.kmk_keyboard import KMKKeyboard
+from kmk.scanners.keypad import KeysScanner
+from kmk.keys import KC
+
+keyboard = KMKKeyboard()
+
+PINS = [board.D8, board.D9, board.D10, board.D11]
+
+keyboard.matrix = KeysScanner(
+    pins=PINS,
+    value_when_pressed=False,
+)
+
+keyboard.keymap = [
+    [KC.SPACE, KC.W, KC.A, KC.D,]
+]
+
+if __name__ == '__main__':
+    keyboard.go()
